d5dee06c82340c21930d27b5b8f9a29d2eb40e92
hy-lin <h.lin@psychologie.uzh.ch>
2018-02-28 17:09:14 +0100

    adding a bit to recall analysis
    there are some missing columns in the data file prevent me from running it further.

--- Analysis/Recall_analysis.r
+++ Analysis/Recall_analysis.r
@@ -15,12 +15,16 @@ determineNumberofUpdating <- function(data){
   max_sz <- max(data$load)
   
   for (trial in 1:n_obs){
+    if ((trial * 100) %% n_obs == 0){
+      print(sprintf('%d', trial * 100 /n_obs))
+    }
+    # print(trial)
     # reset n_updates at the beginning of the new trial
     if (trial == 1){
       n_updates <- rep(0, max_sz)
     }
     else{
-      if (data[trial-1, ]$phase == 'RECALL'){
+      if (data[trial-1, ]$phase == 'RECALL' & data[trial, ]$phase == 'UPDATING'){
         n_updates <- rep(0, max_sz)
       }
     }
@@ -37,8 +41,17 @@ determineNumberofUpdating <- function(data){
   return(data)
 }
 
+raw_data$code <- factor(raw_data$code)
+raw_data$task <- factor(raw_data$task)
 
 raw_data <- read.csv('Data\\updating_numerical.csv', header = TRUE)
 summary(raw_data)
 
 raw_data <- determineNumberofUpdating(raw_data)
+
+recall_data <- raw_data[raw_data$phase == 'RECALL', ]
+
+agg_data <- data.frame(aggregate(list(recall_data$points, recall_data$rt),
+                     list(recall_data$code, recall_data$task, recall_data$load, recall_data$n_updates),
+                     mean))
+names(agg_data) <- c('code', 'task', 'load', 'n_updates', 'PC', 'RT')
